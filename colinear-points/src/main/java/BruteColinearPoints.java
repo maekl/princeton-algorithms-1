305b66d1c020b587b158f27b72ff878dd888ccd8
Magnus Eklund <magnus@bynk.se>
2017-04-06 11:26:29 +0200

    Create project & add handout files

---
+++ colinear-points/src/main/java/BruteColinearPoints.java
@@ -0,0 +1,28 @@
+public class BruteColinearPoints {
+    /**
+     * Finds all line segments containing 4 points
+     *
+     * @param points the points.
+     */
+    public BruteColinearPoints(Point[] points) {
+
+    }
+
+    /**
+     * The number of line segments.
+     *
+     * @return number of segments.
+     */
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    /**
+     * The line sgements.
+     *
+     * @return line segments.
+     */
+    public LineSegment[] segments() {
+        return null;
+    }
+}
